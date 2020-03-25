8867b6a76e06d71f13b7374b1b0af9b3858ec193
Marlon <marmoneymar@icloud.com>
2020-03-25 19:50:13 -0400

    Insert function is done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -11,17 +11,41 @@ class BinarySearchTree:
         self.right = None
 
     # Insert the given value into the tree
-    def insert(self, value):
-        pass
+    def insert(self, newValue):
+        if self.value is None:
+            self.value = newValue
+        elif newValue < self.value:
+            if self.left:
+                self.left.insert(newValue)
+            else:
+                self.left = BinarySearchTree(newValue)
+        else:
+            if self.right:
+                self.right.insert(newValue)
+            else:
+                self.right = BinarySearchTree(newValue)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value is None:
+            return False
+        elif self.value == target:
+            return True
+        else:
+            if target < self.value:
+                self.contains(target)
+            else:
+                self.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right:
+            
+            self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
