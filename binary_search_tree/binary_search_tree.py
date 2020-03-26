e62af31f117c1e38be1f126941019a8990bf5aa9
Marlon <marmoneymar@icloud.com>
2020-03-26 17:20:42 -0400

    refactor

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,12 +9,11 @@ class BinarySearchTree:
         self.value = value
         self.left = None
         self.right = None
+        
 
     # Insert the given value into the tree
     def insert(self, newValue):
-        if self.value is None:
-            self.value = newValue
-        elif newValue < self.value:
+        if newValue < self.value:
             if self.left:
                 self.left.insert(newValue)
             else:
@@ -29,28 +28,28 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        if self.value is None:
-            return False
-        elif self.value == target:
+        if self.value == target:
             return True
-        else:
-            if target < self.value:
-                self.contains(target)
-            else:
-                self.contains(target)
+        if target < self.value and self.left is not None:
+            return self.contains(target)
+        elif target > self.value and self.right is not None:
+            return self.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
         if self.right:
-            
-            self.right.get_max()
+            return self.right.get_max()
         else:
             return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
